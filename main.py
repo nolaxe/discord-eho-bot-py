52d9ab98446c75d55d5ac8bb8a4f3e3b5bceb5d3
nolaxe <exalongd@ya.ru>
2021-06-04 15:57:30 +0500

    ok

--- main.py
+++ main.py
@@ -5,43 +5,31 @@ import json
 import random
 
 client = discord.Client()
-sad_words = ["ЭТБ", "ಠ_ಠ", "ლ(ಠ益ಠლ)", "(ʘᗩʘ')", "ел+", "٩◔̯◔۶", "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻"]
-starter_encouragements = [
- # "хватит есть",
-  #"шо, опять??",
+dict_words = [
+"!1",
+"!2"
+]
+dict_answers2 = [
+  "хватит есть",
+  "шо, опять??",
   "hah этб",
-  "٩◔̯◔۶ этб",
-  "hey сhatterbox! let's hook up ddos this этб chat!"
-  #"Wo ich herkomme, arbeiten alle hart...",  
-  #"Arbeiten!",
-  #"Ein paar Stunden arbeiten wir gern!"
-  # "вчера же ел, не?."
+  "Wo ich herkomme, arbeiten alle hart...",  
+  "Arbeiten!",
+  "Ein paar Stunden arbeiten wir gern!"
+  "вчера же ел, не?."
 ]
 
 def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
+  response = requests.get("https://zenquotes.io/api/random") 
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
-
-@client.event
-async def on_ready():
-  print('Запущен бот {0.user}'.format(client))
-
-@client.event
-
-async def on_member_update (before, after):
-        n = after.JOHN
-     if n:
-     if n.lower().count("Big Brother") > 0:
-        last = before.nick
-        if last:
-            await after.edit(nick=JOHN)
-        else:
-            await after.edit(nick="COOKIES")
-
-
-
+ 
+def get_cat():
+  response = requests.get("https://some-random-api.ml/img/cat")
+  json_data = json.loads(response.text)
+  quote = json_data['link']
+  return(quote)
 
 @client.event
 async def on_message(message):
@@ -49,13 +37,16 @@ async def on_message(message):
     return
   msg = message.content
 
-  if msg.startswith('q1'):
-    quote = get_quote()
+  if msg.startswith('!cat'):
+    quote = get_cat()
     await message.channel.send(quote)
     
-  if any(word in msg for word in sad_words):
-    await message.channel.send(random.choice(starter_encouragements))
+  if any(word in msg for word in dict_words):
+    await message.channel.send(random.choice(dict_answers2))
+
+@client.event
+async def on_ready():
+  print('# эхо + картинки + цитаты #') 
+  print('Запущен бот {0.user}'.format(client))    
 
 client.run(os.getenv('TOKEN'))
-
-
